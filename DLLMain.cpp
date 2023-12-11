fa7d059ee2382a18535f2665b8a6fb3917e32b52
harvs <74739443+h-arvs@users.noreply.github.com>
2023-12-11 22:47:04 +0000

    yeah

--- DLLMain.cpp
+++ DLLMain.cpp
@@ -3,14 +3,160 @@
 #include <dxgi1_4.h>
 #include <iostream>
 #include <assert.h>
+#include <imgui.h>
+#include <imgui_impl_dx12.h>
+#include <imgui_impl_win32.h>
+
+struct FrameContext {
+
+	ID3D12CommandAllocator* commandAllocator = nullptr;
+	ID3D12Resource* main_render_target_resource = nullptr;
+	D3D12_CPU_DESCRIPTOR_HANDLE main_render_target_descriptor;
+
+};
+
+ID3D12Device* device = nullptr;
+UINT buffersCounts = -1;
+FrameContext* frameContext = nullptr;
+
+ID3D12CommandQueue* d3d12CommandQueue = nullptr;
+
+ID3D12DescriptorHeap* d3d12DescriptorHeapImGuiRender = nullptr;
+ID3D12DescriptorHeap* d3d12DescriptorHeapBackBuffers = nullptr;
+ID3D12GraphicsCommandList* d3d12CommandList = nullptr;
+ID3D12CommandAllocator* allocator = nullptr;
 
 typedef HRESULT(__thiscall* present_o)(IDXGISwapChain3*, UINT, UINT);
 present_o presentO;
 
 HRESULT presentH(IDXGISwapChain3* swapchain, UINT syncinterval, UINT flags) {
-	printf("Present");
+	static bool init = false;
+	auto window = FindWindowA(nullptr, "Minecraft");
+	if (SUCCEEDED(swapchain->GetDevice(IID_PPV_ARGS(&device)))) {
+		if (!init) ImGui::CreateContext();
+		DXGI_SWAP_CHAIN_DESC desc;
+		swapchain->GetDesc(&desc);
+		desc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+		desc.OutputWindow = window;
+		desc.Windowed = ((GetWindowLongPtr(window, GWL_STYLE) & WS_POPUP) != 0) ? false : true;
+		buffersCounts = desc.BufferCount;
+
+		frameContext = new FrameContext[buffersCounts];
+
+		D3D12_DESCRIPTOR_HEAP_DESC descriptorImGuiRender = {};
+		descriptorImGuiRender.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+		descriptorImGuiRender.NumDescriptors = buffersCounts;
+		descriptorImGuiRender.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+
+		if (d3d12DescriptorHeapImGuiRender == nullptr) {
+			if (FAILED(device->CreateDescriptorHeap(&descriptorImGuiRender, IID_PPV_ARGS(&d3d12DescriptorHeapImGuiRender)))) {
+				return presentO(swapchain, syncinterval, flags);
+			}
+		}
+
+		if (device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&allocator)) != S_OK) return false;
+
+		for (size_t i = 0; i < buffersCounts; i++) {
+			frameContext[i].commandAllocator = allocator;
+		};
+
+		if (device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, allocator, NULL, IID_PPV_ARGS(&d3d12CommandList)) != S_OK || d3d12CommandList->Close() != S_OK) return false;
+
+		D3D12_DESCRIPTOR_HEAP_DESC descriptorBackBuffers;
+		descriptorBackBuffers.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+		descriptorBackBuffers.NumDescriptors = buffersCounts;
+		descriptorBackBuffers.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+		descriptorBackBuffers.NodeMask = 1;
+
+		if (device->CreateDescriptorHeap(&descriptorBackBuffers, IID_PPV_ARGS(&d3d12DescriptorHeapBackBuffers)) != S_OK)
+			return false;
+
+		const auto rtvDescriptorSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+		D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = d3d12DescriptorHeapBackBuffers->GetCPUDescriptorHandleForHeapStart();
+
+		for (size_t i = 0; i < buffersCounts; i++) {
+			ID3D12Resource* pBackBuffer = nullptr;
+
+			frameContext[i].main_render_target_descriptor = rtvHandle;
+			swapchain->GetBuffer((UINT)i, IID_PPV_ARGS(&pBackBuffer));
+			device->CreateRenderTargetView(pBackBuffer, nullptr, rtvHandle);
+			frameContext[i].main_render_target_resource = pBackBuffer;
+			rtvHandle.ptr += rtvDescriptorSize;
+
+			pBackBuffer->Release();
+		};
+
+		if (!init) {
+			ImGui_ImplWin32_Init(window);
+			ImGui_ImplDX12_Init(device, buffersCounts,
+				DXGI_FORMAT_R8G8B8A8_UNORM, d3d12DescriptorHeapImGuiRender,
+				d3d12DescriptorHeapImGuiRender->GetCPUDescriptorHandleForHeapStart(),
+				d3d12DescriptorHeapImGuiRender->GetGPUDescriptorHandleForHeapStart());
+
+			init = true;
+		}
+
+		if (d3d12CommandQueue == nullptr) return presentO(swapchain, syncinterval, flags);
+
+		ImGui_ImplDX12_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+
+		auto stupidpointer = true;
+		ImGui::ShowDemoWindow(&stupidpointer);
+
+		FrameContext& currentFrameContext = frameContext[swapchain->GetCurrentBackBufferIndex()];
+		currentFrameContext.commandAllocator->Reset();
+
+		D3D12_RESOURCE_BARRIER barrier;
+		barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+		barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+		barrier.Transition.pResource = currentFrameContext.main_render_target_resource;
+		barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+		barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT;
+		barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
+
+		d3d12CommandList->Reset(currentFrameContext.commandAllocator, nullptr);
+		d3d12CommandList->ResourceBarrier(1, &barrier);
+		d3d12CommandList->OMSetRenderTargets(1, &currentFrameContext.main_render_target_descriptor, FALSE, nullptr);
+		d3d12CommandList->SetDescriptorHeaps(1, &d3d12DescriptorHeapImGuiRender);
+
+		ImGui::EndFrame();
+		ImGui::Render();
+
+		ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), d3d12CommandList);
+
+		barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
+		barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
+
+		d3d12CommandList->ResourceBarrier(1, &barrier);
+		d3d12CommandList->Close();
+
+		d3d12CommandQueue->ExecuteCommandLists(1, reinterpret_cast<ID3D12CommandList* const*>(&d3d12CommandList));
+
+		d3d12DescriptorHeapBackBuffers->Release();
+		d3d12CommandList->Release();
+		allocator->Release();
+
+		currentFrameContext.main_render_target_resource->Release();
+		currentFrameContext.commandAllocator->Release();
+
+		device->Release();
+
+		delete frameContext;
+	}
 	return presentO(swapchain, syncinterval, flags);
 }
+
+typedef void(__thiscall* ExecuteCommandListsD3D12)(ID3D12CommandQueue*, UINT, ID3D12CommandList*);
+ExecuteCommandListsD3D12 oExecuteCommandListsD3D12;
+
+void hookExecuteCommandListsD3D12(ID3D12CommandQueue* queue, UINT NumCommandLists, ID3D12CommandList* ppCommandLists) {
+	if (!d3d12CommandQueue)
+		d3d12CommandQueue = queue;
+
+	oExecuteCommandListsD3D12(queue, NumCommandLists, ppCommandLists);
+};
 
 void init() {
 	AllocConsole();
@@ -18,6 +164,7 @@ void init() {
 	freopen_s(&f, "CONOUT$", "w", stdout); // will only work if u have sum developer flags or smth
 	if (kiero::init(kiero::RenderType::D3D12) == kiero::Status::Success) {
 		kiero::bind(140, (void**)&presentO, presentH);
+		kiero::bind(54, (void**)&oExecuteCommandListsD3D12, hookExecuteCommandListsD3D12);
 	}
 }
 
